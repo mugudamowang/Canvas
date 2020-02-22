bc36fdfd88c7ba1ab0e5908b1b6205526603d5af
mugu <1175233520@qq.com>
2020-02-22 15:07:08 +0800

    add texture,fullscrenn,color tweak, blackboard2.0

--- main.js
+++ main.js
@@ -5,14 +5,28 @@ var lastpoint = {
     'x': undefined,
     'y': undefined
 }
+var pagewidth = document.documentElement.clientWidth
+var pageheight = document.documentElement.clientHeight//获取页面宽高
+canvas.width = pagewidth
+canvas.height = pageheight
 
-function drawLine(canvas){
+window.onresize = function (){
+    
+    var pagewidth = document.documentElement.clientWidth
+    var pageheight = document.documentElement.clientHeight//获取页面宽高
+    
+    canvas.width = pagewidth
+    canvas.height = pageheight
+}
+
+function drawLine(canvas,linewidth){
 
     var x = canvas.clientX
     var y = canvas.clientY
     var newpoint = {'x':x, 'y':y}
-    ctx.strokeStyle = 'white'
+    ctx.strokeStyle = '#fdfdfd'
     ctx.beginPath()
+    ctx.lineWidth = linewidth 
     ctx.moveTo(lastpoint.x,lastpoint.y)
     ctx.lineTo(newpoint.x,newpoint.y)
     ctx.stroke()
@@ -21,25 +35,25 @@ function drawLine(canvas){
 }
 
 //f 画圈
-function drawCricle(radius) {
-    var x = canvas.clientX
-    var y = canvas.clientY
+function drawCricle(pendown,radius) {
+    var x = pendown.clientX
+    var y = pendown.clientY
     ctx.beginPath()
-    ctx.fillStyle = 'white'
+    ctx.fillStyle = '#fdfdfd'
     ctx.arc(x, y, radius, 0, Math.PI * 2)
     ctx.fill()
 }
 //按下去
-canvas.onmousedown = function (canvas) {
+canvas.onmousedown = function (pendown) {
     state = true
-    drawCricle(1)
+    drawLine(pendown,7)//补充间隙
+    drawCricle(pendown,3)//粉笔开始效果
 }
-
 //移动鼠标
 
-canvas.onmousemove = function (canvas) {
+canvas.onmousemove = function (penmove) {
     if (state) {
-        drawLine(canvas)
+        drawLine(penmove,7)
     }else{
 
     }
@@ -48,7 +62,9 @@ canvas.onmousemove = function (canvas) {
 
 //松开鼠标
 
-canvas.onmouseup = function (canvas) {
+canvas.onmouseup = function (penup) {
+    drawLine(penup,7)
+    drawCricle(penup,3)
     state = false
     lastpoint = {
         'x': undefined,
