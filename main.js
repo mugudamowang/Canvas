ab381cd5f680f1621f997fc468cb679469fe9f54
mugu <1175233520@qq.com>
2020-02-23 20:29:52 +0800

    add eraser && pen bloackboard2.3

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //variable-----------------------------
 var canvas = document.getElementById('mugu')
 var ctx = canvas.getContext('2d');
-var pencolor = document.getElementsByTagName('li')
-var pen = 'white'
-var state = false
+var pen = document.getElementsByTagName('li')
+var pencolor = 'white'
+var penable = false
 var lastpoint = {
     'x': undefined,
     'y': undefined
@@ -11,58 +11,67 @@ var lastpoint = {
 
 
 
-//设置颜色
-choseColor()
+//设置颜色 操作
+choseColor(pen)
 
-//设置画布大小
-setCanvasSize()
+//设置画布大小 操作
+setCanvasSize(canvas)
 window.onresize = function () {
-    this.setCanvasSize()
+    this.setCanvasSize(canvas)
 }
 
-//设置拾色器插件
+//设置拾色器 插件
 Colorpicker.create({
     bindClass: 'picker',
     change: function (elem, hex) {
         elem.style.backgroundColor = hex
-        elem.id =hex
-        pen = hex 
+        elem.id = hex
+        pencolor = hex
     }
-    
+
 })
 
+//监听鼠标 操作
+listenTomouse(canvas)
 
-
-
-
+//工具切换 状态
+var eraserable = false
+eraser.onclick = function () {
+    eraserable = true
+}
+drawpen.onclick = function () {
+    eraserable = false
+}
 
 
 
 //function----------------------------------------------------------------
 
+//圆形橡皮檫
 
 //色卡选择
-function choseColor(){
-    for(index = 0;index <3 ;index++){
-        pencolor[index].onclick = function (xxx) {
-            pen = xxx.toElement.id
+function choseColor(pen) {
+    for (index = 0; index < 3; index++) {
+        pen[index].onclick = function (xxx) {
+            pencolor = xxx.toElement.id
         }
     }
 }
 
-
-function setCanvasSize() {
+//尺寸设置函数
+function setCanvasSize(canvas) {
     var pagewidth = document.documentElement.clientWidth
     var pageheight = document.documentElement.clientHeight//获取页面宽高
     canvas.width = pagewidth
     canvas.height = pageheight
 }
 
+//画线函数
 function drawLine(canvas, linewidth) {
     var x = canvas.clientX
     var y = canvas.clientY
     var newpoint = { 'x': x, 'y': y }
-    ctx.strokeStyle = pen
+    ctx.strokeStyle = pencolor
     ctx.beginPath()
     ctx.lineWidth = linewidth
     ctx.moveTo(lastpoint.x, lastpoint.y)
@@ -72,40 +81,59 @@ function drawLine(canvas, linewidth) {
     lastpoint = newpoint
 }
 
-//f 画圈
+//画圈f
 function drawCricle(pendown, radius) {
     var x = pendown.clientX
     var y = pendown.clientY
     ctx.beginPath()
-    ctx.fillStyle = pen
+    ctx.fillStyle = pencolor
     ctx.arc(x, y, radius, 0, Math.PI * 2)
     ctx.fill()
 }
-//按下去
-canvas.onmousedown = function (pendown) {
-    state = true
-    drawLine(pendown, 7)//补充间隙
-    drawCricle(pendown, 3)//粉笔开始效果
-}
-//移动鼠标
 
-canvas.onmousemove = function (penmove) {
-    if (state) {
-        drawLine(penmove, 7)
-    } else {
+//监听事件
+function listenTomouse(canvas) {
+
+    //按下去
+    canvas.onmousedown = function (pendown) {
+        var x = pendown.clientX
+        var y = pendown.clientY
+        penable = true
+
+
+        if (eraserable) {
+            ctx.clearRect(x-7.5,y-7.5,15,15)
+        } else {
+
+            drawLine(pendown, 7)//补充间隙
+            drawCricle(pendown, 3)//粉笔开始效果
+        }
 
     }
-}
 
+    //移动鼠标
+    canvas.onmousemove = function (penmove) {
+        var x = penmove.clientX
+        var y = penmove.clientY
 
-//松开鼠标
+        if (!penable) { return }
 
-canvas.onmouseup = function (penup) {
-    drawLine(penup, 7)
-    drawCricle(penup, 3)
-    state = false
-    lastpoint = {
-        'x': undefined,
-        'y': undefined
+        if (eraserable) {
+            ctx.clearRect(x-7.5,y-7.5,20,20)
+        } else {
+            drawLine(penmove, 7)
+        }
+    }
+    //松开鼠标
+    canvas.onmouseup = function (penup) {
+        if (!penable) {
+            drawLine(penup, 7)
+            drawCricle(penup, 3)
+        }
+        penable = false
+        lastpoint = {
+            'x': undefined,
+            'y': undefined
+        }
     }
 }
