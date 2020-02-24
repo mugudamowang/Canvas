1abbff7aedc31d7a062c2236651ec23a8799a9bf
mugu <1175233520@qq.com>
2020-02-24 13:08:41 +0800

    add css style and ahjust

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 var canvas = document.getElementById('mugu')
 var ctx = canvas.getContext('2d');
 var pen = document.getElementsByTagName('li')
-var pencolor = 'white'
 var penable = false
+var pencolor = 'white'
 var lastpoint = {
     'x': undefined,
     'y': undefined
@@ -13,6 +13,7 @@ var lastpoint = {
 
 //设置颜色 操作
 choseColor(pen)
+
 
 //设置画布大小 操作
 setCanvasSize(canvas)
@@ -46,12 +47,12 @@ drawpen.onclick = function () {
     drawpen.classList.add('ative')
     eraser.classList.remove('ative')
 }
-clearkit.onclick = function (){
-    ctx.clearRect(0,0,canvas.width,canvas.height)
+clearkit.onclick = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
-saveimg.onclick = function (){
+saveimg.onclick = function () {
     var url = canvas.toDataURL('image/png')
-    var a  = document.createElement('a')
+    var a = document.createElement('a')
     document.body.appendChild(a)
     a.href = url
     a.download = '宁吃了吗'
@@ -160,48 +161,48 @@ function listenTomouse(canvas) {
                 'y': undefined
             }
         }
-} else {
-    //按下去
-    canvas.onmousedown = function (pendown) {
-        var x = pendown.clientX
-        var y = pendown.clientY
-        penable = true
+    } else {
+        //按下去
+        canvas.onmousedown = function (pendown) {
+            var x = pendown.clientX
+            var y = pendown.clientY
+            penable = true
 
 
-        if (eraserable) {
-            ctx.clearRect(x - 7.5, y - 7.5, 15, 15)
-        } else {
+            if (eraserable) {
+                ctx.clearRect(x - 7.5, y - 7.5, 15, 15)
+            } else {
 
-            drawLine(pendown, 7)//补充间隙
-            drawCricle(pendown, 3)//粉笔开始效果
+                drawLine(pendown, 7)//补充间隙
+                drawCricle(pendown, 3)//粉笔开始效果
+            }
+
         }
 
+        //移动鼠标
+        canvas.onmousemove = function (penmove) {
+            var x = penmove.clientX
+            var y = penmove.clientY
+
+            if (!penable) { return }
+
+            if (eraserable) {
+                ctx.clearRect(x - 7.5, y - 7.5, 20, 20)
+            } else {
+                drawLine(penmove, 7)
+            }
+        }
+        //松开鼠标
+        canvas.onmouseup = function (penup) {
+            if (!penable) {
+                drawLine(penup, 7)
+                drawCricle(penup, 3)
+            }
+            penable = false
+            lastpoint = {
+                'x': undefined,
+                'y': undefined
+            }
+        }
     }
-
-    //移动鼠标
-    canvas.onmousemove = function (penmove) {
-        var x = penmove.clientX
-        var y = penmove.clientY
-
-        if (!penable) { return }
-
-        if (eraserable) {
-            ctx.clearRect(x - 7.5, y - 7.5, 20, 20)
-        } else {
-            drawLine(penmove, 7)
-        }
-    }
-    //松开鼠标
-    canvas.onmouseup = function (penup) {
-        if (!penable) {
-            drawLine(penup, 7)
-            drawCricle(penup, 3)
-        }
-        penable = false
-        lastpoint = {
-            'x': undefined,
-            'y': undefined
-        }
-    }
-}
 }
